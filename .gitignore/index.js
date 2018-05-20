ca420fc4a3f5eaf93e8c20c356bea3ece187fcdd
Ilian0112 <38587349+Ilian0112@users.noreply.github.com>
2018-05-20 14:25:08 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,28 +1,89 @@
-var Discord = require('discord.js');
-var bot = new Discord.Client();
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+const PREFIX = ".";
+const queue = new Map();
 const request = require("request");
+const EVERYONE = "@";
 let prefix = "."
 let prefixLog = "[!]"
- 
-console.log("------------------------------")
-console.log("Démarrage...");
- 
+var client = new Discord.Client();
+
+var bot = new Discord.Client();
+
+var servers = {};
+
+bot.on('ready', () => {
+
+    bot.user.setActivity(prefix + "help | Démarré et prêt !");
+    console.log("------------------------------")
+    console.log(prefixLog + " Bot créé par Ilian ! <3")
+    console.log(prefixLog + " Bot prêt")
+    console.log("------------------------------")
+
+    setTimeout(state1, 1000);
+
+})
+
+function state1() {
+    request("http://api.radionomy.com/currentaudience.cfm?radiouid=5d198d45-3ee5-4dee-8182-4ee0184d41f1&apikey=15355fc0-4344-4ff7-a795-8efa38742083", (error, response, body) => {
+        if (error) return console.log(error);
+
+        if (body == undefined) {
+            bot.user.setActivity("?");
+        } else {
+            var msgActivity;
+            if (parseInt(body) < 2) {
+                msgActivity = "auditeur"
+            } else {
+                msgActivity = "auditeurs"
+            }
+            bot.user.setActivity(".help | " + body + "" + msgActivity);
+            setTimeout(state2, 30000);
+        }
+    })
+}
+
+function state2() {
+    bot.user.setActivity(prefix + "help | " + bot.guilds.size + " serveurs, " + bot.users.size + " membres");
+    setTimeout(state3, 5000);
+}
+
+function state3() {
+    bot.user.setActivity(prefix + "help | Par Ilian ! ^^");
+    setTimeout(state1, 5000);
+}
+
+bot.on('message', function (msg) {
+    if (msg.content.indexOf(prefix) === 0) {
+        var cmdTxt = msg.content.split(" ")[0].substring(prefix.length);
+        var cmd = commands[cmdTxt];
+        var suffix = msg.content.substring(cmdTxt.length + prefix.length + 1);
+        if (cmd !== undefined) {
+            cmd.process(msg, suffix);
+        } else {
+            cmdTxt = cmdTxt.replace('`', '');
+            if (cmdTxt === '') {
+                var cmdTxt = "none";
+            }
+        }
+    }
+});
+
 var commands = {
     "join": {
         process: function (msg, suffix) {
             const channel = msg.member.voiceChannel;
             if (!channel) return msg.channel.send(':warning: | **Tu est pas dans un salon vocal.**');
-            if(!msg.member.voiceChannel.joinable) {
+            if (!msg.member.voiceChannel.joinable) {
                 msg.channel.send(":warning: | **Je n'ai pas les permissions suffisantes pour jouer de la musique sur ce canal..**");
                 return;
             }
             msg.member.voiceChannel.join();
             msg.channel.send(":loudspeaker: | **Je suis là !**");
-           
-            console.log("La commande 'help' a été exécutée par " + msg.author.username + " sur le serveur " + guild.name)
+
         }
     },
-   
+
     "play": {
         process: function (msg, suffix) {
             const channel = msg.member.voiceChannel;
@@ -30,27 +91,26 @@ var commands = {
             if (suffix) {
                 if (suffix === "Radio" || suffix === "radio") {
                     msg.channel.send(":musical_note:  |  **Radio Modern**");
-                    var radio = "RadioModern";                 
+                    var radio = "RadioModern";
                 } else {
                     msg.channel.send(":warning:  |  **Erreur**, la commande que vous souhaitez taper est ``.play radio``");
                     return;
                 }
                 msg.member.voiceChannel.join().then(connection => {
-                    require('http').get("http://streaming.radionomy.com/"+radio, (res) => {
+                    require('http').get("http://streaming.radionomy.com/" + radio, (res) => {
                         connection.playStream(res);
                     })
                 })
-                .catch(console.error);
+                    .catch(console.error);
             } else {
                 msg.channel.send(":warning: | **Erreur**, la commande que vous souhaitez taper est ``.play radio``");
             }
-       
-        console.log("La commande 'play' a été exécutée par " + msg.author.username + " sur le serveur " + msg.guild.name)
-   
+
+
         },
- 
+
     },
-       
+
     "stop": {
         process: function (msg, suffix) {
             const voiceChannel = msg.member.voiceChannel;
@@ -60,12 +120,11 @@ var commands = {
             } else {
                 msg.channel.send(":warning: | **Je ne suis pas dans un salon vocal.**");
             }
-       
-        console.log("La commande 'stop' a été exécutée par " + msg.author.username + " sur le serveur " + msg.guild.name)
- 
+
+
         }
     },
-   
+
     "help": {
         process: function (msg, suffix) {
             var help_embed = new Discord.RichEmbed()
@@ -82,12 +141,11 @@ var commands = {
                 .setFooter("Par Ilian ! ^^")
                 .setAuthor("Message d'aide")
                 .setTimestamp()
-                msg.channel.sendEmbed(help_embed)
-               
-        console.log("La commande 'help' a été exécutée par " + msg.author.username + " sur le serveur " + msg.guild.name)
+            msg.channel.sendEmbed(help_embed)
+
         },
     },
-   
+
     "botinfo": {
         process: function (msg, suffix) {
             var ping_embed = new Discord.RichEmbed()
@@ -110,70 +168,58 @@ var commands = {
                 .addBlankField()
                 .setTimestamp()
                 .setFooter("By Ilian^^ !")
-               
-        console.log("La commande 'help' a été exécutée par " + msg.author.username + " sur le serveur " + msg.guild.name)
+
         }
     },
 }
- 
-bot.on('ready', () => {
-   
-    bot.user.setActivity(prefix + "help | Démarré et prêt !");
-    console.log("------------------------------")
-    console.log(prefixLog + " Bot créé par Ilian ! <3")
-    console.log(prefixLog + " Bot prêt")
-    console.log("------------------------------")
-   
-    setTimeout(state1, 1000);
- 
-})
-   
-function state1() {
-    request("http://api.radionomy.com/currentaudience.cfm?radiouid=5d198d45-3ee5-4dee-8182-4ee0184d41f1&apikey=15355fc0-4344-4ff7-a795-8efa38742083", (error, response, body) => {
-        if (error) return console.log(error);
- 
-        if (body == undefined) {
-            bot.user.setActivity("?");
-        } else {
-            var msgActivity;
-        if (parseInt(body) < 2) {
-            msgActivity = "auditeur"
-        } else {
-            msgActivity = "auditeurs"
-        }
-        bot.user.setActivity(".help | " + body + "" + msgActivity);
-        setTimeout(state2, 30000);
-        }
-    })
-}
- 
-function state2 () {
-    bot.user.setActivity(prefix + "help | " + bot.guilds.size + " serveurs, " + bot.users.size + " membres");
-    setTimeout(state3, 5000);
-}
- 
-function state3 () {
-    bot.user.setActivity(prefix + "help | Par Ilian ! ^^");
-    setTimeout(state1, 5000);
-}
- 
-bot.on('message', function (msg) {
-    if(msg.content.indexOf(prefix) === 0) {
-        var cmdTxt = msg.content.split(" ")[0].substring(prefix.length);
-        var cmd = commands[cmdTxt];
-        var suffix = msg.content.substring(cmdTxt.length + prefix.length+1);
-        if(cmd !== undefined) {
-            cmd.process(msg, suffix);
-        } else {
-            cmdTxt = cmdTxt.replace('`', '');
-            if (cmdTxt === ''){
-                var cmdTxt = "none";
-            }
-            msg.channel.send(":warning: | **La commande** `" + cmdTxt + "` **n'existe pas** `" + prefix + "help`");
-            console.log(msg.author.username + " a tenté de taper une commande... RT, sur le serveur " + msg.guild.name)
-        }
+
+bot.on("message", async function (message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    var args2 = message.content.split(" ").slice(1);
+
+    var suffix = args2.join(" ");
+
+    var reason = args2.slice(1).join(" ");
+
+    var reasontimed = args2.slice(2).join(' ')
+
+    var user = message.mentions.users.first();
+
+    var guild = message.guild;
+
+    var member = message.member;
+
+    var user = message.mentions.users.first();
+
+    switch (args[0].toLowerCase()) {
+        case "send":
+            let xoargs = message.content.split(" ").slice(1);
+            let suffix = xoargs.join(' ')
+            var xo02 = message.guild.channels.find('name', 'send-promotion');
+            if (message.channel.name !== 'send-promotion') return message.reply("Commande a effectuer dans #send-promotion")
+            if (!suffix) return message.reply("Merci d'écrire un message à envoyé à Tard0s.")
+            var vc_embed = new Discord.RichEmbed()
+                .setColor("#04B404")
+                .addField(message.author.username + "#" + message.author.discriminator + " – Promotion", suffix)
+                .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬", "Provenance du message : ``" + message.guild.name + "``", true)
+                .setThumbnail(message.guild.iconURL)
+                .setFooter("Ilian's Corporation")
+                .setTimestamp();
+
+            let Tard0sTV = bot.users.find("323039726040776705", "id")
+            if (!Tard0sTV) return console.log("Tard0s non trouvé")
+            Tard0sTV.createDM().then(channel => {
+                channel.send(vc_embed)
+            })
+            //bot.channels.findAll('name', 'send-promotion').map(channel => channel.send(vc_embed));
+            break;
+
     }
 });
- 
  
 bot.login(process.env.TOKEN);
