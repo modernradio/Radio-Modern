b10786f7161f66103af6d848f7ba7eb979366296
Radio Modern <37845826+modernradio@users.noreply.github.com>
2018-05-31 19:10:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -61,17 +61,7 @@ function state2() {
     request("http://api.radionomy.com/currentsong.cfm?radiouid=5d198d45-3ee5-4dee-8182-4ee0184d41f1&apikey=15355fc0-4344-4ff7-a795-8efa38742083", (error, response, body) => {
         if (error) return console.log(error);
 
-        if (body == "Advert:TargetSpot - Advert:Targetspot") {
-            var msgActivity;
-            msgActivity = "Publicité..."
-            console.log("-> Publicité...");
-            console.log("------------------------------");
-        } else {
-            var msgActivity;
-            msgActivity = body
-        };
-
-        bot.user.setActivity('"' + msgActivity + '"');
+        bot.user.setActivity(prefix + 'help "' + body + '"');
         setTimeout(state3, 5000);
     })
 }
