9c9d69eb77b66a18f0741b2753580ab1843192cd
Radio Modern <37845826+modernradio@users.noreply.github.com>
2018-08-24 15:08:17 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,35 +8,35 @@ bot.on("ready", () => {
 
 function autoplayradio () {
 
-    var channel_ilian = bot.channels.find("id", "481202105382862848");
-    var channel_supers = bot.channels.find("id", "444201909666971648");
-    var channel_qz = bot.channels.find("id", "442651081080569867");
+    var channel_bar = bot.channels.find("id", "482530580123222044");
+    //var channel_libre = bot.channels.find("id", "");
+    //var channel_libre = bot.channels.find("id", "");
     
     autoplayradio_join();
 
     function autoplayradio_join () {
-        channel_ilian.join().then(connection => {
+        channel_bar.join().then(connection => {
             require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
                 connection.playStream(res);
             })
         })
-        channel_supers.join().then(connection => {
+        /*channel_libre.join().then(connection => {
             require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
                 connection.playStream(res);
             })
         })
-        channel_qz.join().then(connection => {
+        channel_libre.join().then(connection => {
             require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
                 connection.playStream(res);
             })
-        })
+        })*/
         setTimeout(autoplayradio_leave, 15 * 60 * 1000)
     }
 
     function autoplayradio_leave () {
         channel_ilian.leave();
-        channel_supers.leave();
-        channel_qz.leave();
+        //channel_libre.leave();
+        //channel_libre.leave();
         autoplayradio_join();
     }
 }
