1127447ae2f8a0a34b4a1362d467101e7c7b30bf
Radio Modern <37845826+modernradio@users.noreply.github.com>
2018-08-29 12:28:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,7 +10,7 @@ function autoplayradio () {
 
     var channel_bar = bot.channels.find("id", "482530580123222044");
     var channel_imagi = bot.channels.find("id", "480886933115895809");
-    //var channel_libre = bot.channels.find("id", "");
+    var channel_arcadia = bot.channels.find("id", "450189683398279168");
     
     autoplayradio_join();
 
@@ -25,18 +25,18 @@ function autoplayradio () {
                 connection.playStream(res);
             })
         })
-        /*channel_libre.join().then(connection => {
+        channel_arcadia.join().then(connection => {
             require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
                 connection.playStream(res);
             })
-        })*/
+        })
         setTimeout(autoplayradio_leave, 15 * 60 * 1000)
     }
 
     function autoplayradio_leave () {
-        channel_ilian.leave();
+        channel_bar.leave();
         channel_imagi.leave();
-        //channel_libre.leave();
+        channel_arcadia.leave();
         autoplayradio_join();
     }
 }
